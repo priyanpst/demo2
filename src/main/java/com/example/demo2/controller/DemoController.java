97f6c78a53cb9f7cef3a5bccf1613a5583fc4a18
priyanka-kumari <priyanka.kumari@npstx.com>
2024-02-27 17:52:23 +0530

    chnages in controller

--- src/main/java/com/example/demo2/controller/DemoController.java
+++ src/main/java/com/example/demo2/controller/DemoController.java
@@ -3,7 +3,7 @@ package com.example.demo2.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/demo2-0.0.1-SNAPSHOT")
 public class DemoController {
 
     @GetMapping("/demo")
