f78252b2c56003da0e2a6d958a9f74bd8c3cd028
priyanka-kumari <priyanka.kumari@npstx.com>
2024-02-28 12:08:19 +0530

    changes for tomcat

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,13 +2,22 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/api/controller")
 public class Demo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 		System.out.println("-------application started-------------------------");
+	}
+
+	@GetMapping("/demo")
+	public String getDemo(){
+		System.out.println("inside getDemo method");
+		return "DEMO";
 	}
 
 }
--- src/main/java/com/example/demo2/controller/DemoController.java
+++ src/main/java/com/example/demo2/controller/DemoController.java
@@ -3,11 +3,12 @@ package com.example.demo2.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/demo2-0.0.1-SNAPSHOT")
+@RestController()
 public class DemoController {
 
-    @GetMapping("/demo")
+    @GetMapping("/demo2")
     public String getDemo(){
+        System.out.println("inside getDemo method");
         return "DEMO";
     }
 
